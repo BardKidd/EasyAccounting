27a76e411c7b33ca911f357117abe2c4d96c487f
Valens Rin <mobilesuit0922@gmail.com>
2025-12-05 09:47:52 +0800

    feat: 新增預設記帳類別種子資料並更新相關依賴。

--- packages/shared/src/constants/index.ts
+++ packages/shared/src/constants/index.ts
@@ -40,6 +40,63 @@ export enum DetailType {
   ALCOHOL = '酒類',
   FRUIT = '水果',
 
+  // 交通細項
+  BUS = '公車',
+  MRT = '捷運',
+  TAXI = '計程車',
+  TRAIN = '火車/高鐵',
+  GAS = '加油',
+  PARKING = '停車',
+  TOLL = '過路費',
+  MAINTENANCE = '維修保養',
+
+  // 娛樂細項
+  MOVIE = '電影',
+  GAME = '遊戲',
+  PARTY = '聚會',
+  TRAVEL = '旅遊',
+  MUSIC = '音樂',
+  SUBSCRIPTION = '訂閱服務',
+
+  // 購物細項
+  CLOTHING = '服飾',
+  ELECTRONICS = '3C產品',
+  GROCERIES = '生活用品',
+  BEAUTY = '美妝保養',
+
+  // 個人細項
+  HAIRCUT = '剪髮',
+  COSMETICS = '化妝品',
+  GYM = '健身',
+
+  // 醫療細項
+  MEDICINE = '藥品',
+  DOCTOR = '看診',
+  INSURANCE = '保險',
+
+  // 家居細項
+  RENT = '房租',
+  UTILITIES = '水電瓦斯',
+  INTERNET = '網路費',
+
+  // 家庭細項
+  CHILD = '小孩',
+  PET = '寵物',
+  ELDER = '孝親費',
+
+  // 生活細項
+  PHONE = '電話費',
+
+  // 學習細項
+  BOOKS = '書籍',
+  COURSE = '課程',
+  TUITION = '學費',
+
+  // 其他細項
+  DONATION = '捐款',
+  TAX = '稅金',
+  FEE = '手續費',
+
   // 轉帳細項
   TRANSFER = '轉帳',
   WITHDRAWAL = '提款',
