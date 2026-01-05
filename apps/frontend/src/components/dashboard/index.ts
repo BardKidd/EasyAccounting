0d00fd84495f55676046e18f04322495e3be0af5
Valens Rin <mobilesuit0922@gmail.com>
2026-01-05 17:00:07 +0800

    refactor: 調整 AssetTrendChart 導入路徑以符合命名慣例

--- apps/frontend/src/components/dashboard/index.ts
+++ apps/frontend/src/components/dashboard/index.ts
@@ -1,6 +1,6 @@
 import RecentTransactions from './recentTransactions';
 import SummaryCards from './summaryCards';
 import TrendChart from './trendChart';
-import AssetTrendChart from './AssetTrendChart';
+import AssetTrendChart from './assetTrendChart';
 
 export { RecentTransactions, SummaryCards, TrendChart, AssetTrendChart };
